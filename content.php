ce16a59b45187ee4c987943447cc96d930842094
tomarinrc <tarce@ibg.local>
2016-06-09 16:15:48 -0500

    Call To Action Animations

--- content.php
+++ content.php
@@ -16,12 +16,18 @@
   <?php if ($pos=strpos($post->post_content, '<!--more-->')) { ?>
     <section class="post-top-angle post-top-angle-call"></section>
     <a href="<?php the_permalink() ?>" class="post-read-more-link">
-      <div class="post-read-more">
+      <div class="post-read-more post-read-more-effect" data-letters="Let's Go!">
         READ MORE
       </div>
     </a>
     <section class="post-bottom-angle"></section>
   <?php } else { ?>
-  <section class="post-bottom-angle  post-bottom-angle-call"></section>
+    <section class="post-top-angle midblue"></section>
+    <a href="<?php the_permalink() ?>" class="post-read-more-link">
+      <div class="post-read-more post-read-more-effect midblue" data-letters="Woo Let's Talk!">
+        Discuss
+      </div>
+    </a>
+    <section class="post-bottom-angle midblue"></section>
   <?php } ?>
 </article>
